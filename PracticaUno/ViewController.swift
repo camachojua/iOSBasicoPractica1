99f1210f801bb06de06e1e6308c575712831bedb
Juan Alberto Camacho Bolaños <juancamacho@ciencias.unam.mx>
2021-02-19 16:19:11 -0600

    Initial Commit

---
+++ PracticaUno/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticaUno
+//
+//  Created by Juan Alberto Camacho Bolaños on 19/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
